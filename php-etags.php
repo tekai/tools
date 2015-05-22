dd9b7163b3879da4ac05b299449f4ad97b99ca30
Engelke Eschner <tekai@gmx.li>
2015-05-22 18:10:10 +0200

    Support for namespaces & const

--- php-etags.php
+++ php-etags.php
@@ -40,10 +40,11 @@ suche => suchbegriff
 function tag_file($file) {
     $defs         = array();
     $offset       = 0;
-    $function     = false;
+    $functionp    = false;
     $class        = false;
     $define       = false;
     $stringp      = false;
+    $constp       = false;
     $extends      = false;
     $className    = '';
     $constructorp = false;
@@ -63,38 +64,42 @@ function tag_file($file) {
         foreach ($tokens as &$t) {
 
             if (is_array($t)) {
-                if ($t[0] == T_NAMESPACE) {
+                if ($t[0] === T_NAMESPACE) {
                     $nsp = true;
                     $ns_arr = array();
                     $namespace = '';
                 }
-                elseif ($nsp && $t[0] == T_STRING) {
+                elseif ($nsp && $t[0] === T_STRING) {
                     $ns_arr[] = $t[1];
                 }
-                elseif ($t[0] == T_FUNCTION) {
-                    $function = true;
+                elseif ($t[0] === T_FUNCTION) {
+                    $functionp = true;
                     $def = array('line' => $t[2], 'offset' => $offset);
                 }
-                elseif ($t[0] == T_CLASS) {
+                elseif ($t[0] === T_CLASS) {
                     $class = true;
                     $className = '';
                     $classdef = array('line' => $t[2], 'offset' => $offset);
                     $constructorp = false;
                 }
-                elseif ($t[0] == T_EXTENDS) {
+                elseif ($t[0] === T_EXTENDS) {
                     $extends = true;
                 }
-                elseif ($extends && $t[0] == T_STRING) {
+                elseif ($t[0] === T_CONST) {
+                    $constp = true;
+                    $def = array('line' => $t[2], 'offset' => $offset);
+                }
+                elseif ($extends && $t[0] === T_STRING) {
                     $extends = false;
                 }
-                elseif ($class && !$curly && !$className && $t[0] == T_STRING) {
+                elseif ($class && !$curly && !$className && $t[0] === T_STRING) {
                     $className = $t[1];
                     // hmm merken wo die klasse anfaengt und wenn
                     // $constructorp == false am ende der Klasse/Schleife
                     // dann Klassendef als constructor
                 }
                 // class or function/method name
-                elseif ($function && $t[0] == T_STRING) {
+                elseif ($functionp && $t[0] === T_STRING) {
                     if ($class && $curly && ($t[1] == '__construct' ||
                                              $t[1] == $className)) {
                         $type = 'constructor';
@@ -116,28 +121,49 @@ function tag_file($file) {
                     }
                     if ($type == 'constructor') {
                         $def['name'] = $type.' '.$className;
+                        if ($namespace) {
+                            $def['ns-name'] = $type.' '.$namespace.$className;
+                        }
+                    }
+                    elseif ($type == 'method') {
+                        $def['name'] = $type.' '.$t[1];
                     }
                     else {
                         $def['name'] = $type.' '.$t[1];
+                        if ($namespace) {
+                            $def['ns-name'] = $type.' '.$namespace.$t[1];
+                        }
                     }
-                    if ($function) {
-                        $defs[$def['name']] = ''
-                            .$def['search'].chr(127)
-                            .$def['name'].chr(1)
-                            .$def['line'].','
-                            .$def['offset']."\n";
+                    if ($functionp) {
+                        add_def($defs, $def);
                         $def = array();
-                        $function = false;
+                        $functionp = false;
                     }
 
                 }
                 // T_STRING define
-                elseif ($t[0] == T_STRING && $t[1] == 'define') {
+                elseif ($t[0] === T_STRING && $t[1] == 'define') {
                     $define = true;
                     $def = array('line' => $t[2], 'offset' => $offset);
                 }
+                elseif ($constp && $t[0] === T_STRING) {
+                    $def['search'] = 'const '.$t[1];
+                    preg_match('/^(.*)'.preg_quote($t[1],'/').'/', $lines[$t[2]-1], $m);
+                    if ($m) {
+                        $def['search'] = $m[0];
+                        // offset has to point to the start of the line
+                        $def['offset'] -= strlen($m[1]) - 6;
+                    }
+                    $def['name'] = $t[1];
+                    if ($namespace) {
+                        $def['ns-name'] = $namespace.$t[1];
+                    }
+                    add_def($defs, $def);
+                    $def = array();
+
+                }
                 // T_CONSTANT_ENCAPSED_STRING aka 'string' or "string"
-                elseif ($t[0] == T_CONSTANT_ENCAPSED_STRING && $define) {
+                elseif ($t[0] === T_CONSTANT_ENCAPSED_STRING && $define) {
 
                     $define = false;
                     $def['search'] = 'define('.$t[1];
@@ -148,11 +174,7 @@ function tag_file($file) {
                         $def['offset'] -= strlen($m[1]);
                     }
                     $def['name'] = substr($t[1],1,-1);
-                    $defs[$def['name']] = ''
-                        .$def['search'].chr(127)
-                        .$def['name'].chr(1)
-                        .$def['line'].','
-                        .$def['offset']."\n";
+                    add_def($defs, $def);
                     $def = array();
 
                 }
@@ -192,18 +214,16 @@ function tag_file($file) {
                                 $classdef['offset'] -= strlen($m[1]);
                             }
                             $classdef['name'] = $type.' '.$className;
-                            $defs[$classdef['name']] = ''
-                                .$classdef['search'].chr(127)
-                                .$classdef['name'].chr(1)
-                                .$classdef['line'].','
-                                .$classdef['offset']."\n";
-
+                            if ($namespace) {
+                                $classdef['ns-name'] = $type.' '.$namespace.$className;
+                            }
+                            add_def($defs, $classdef);
                         }
                     }
                 }
                 // anonymous function
-                elseif ($t == '(' && $function) {
-                    $function = false;
+                elseif ($t == '(' && $functionp) {
+                    $functionp = false;
                     $def = array();
                 }
                 elseif ($t == ';') {
@@ -211,6 +231,7 @@ function tag_file($file) {
                         $nsp = false;
                         $namespace = implode('\\', $ns_arr).'\\';
                     }
+                    $constp = false;
                 }
 
                 $offset += strlen($t);
@@ -219,11 +240,28 @@ function tag_file($file) {
         if (!empty($defs)) {
             ksort($defs);
             $tags = join("", $defs);
-            unset($defs);
+            $defs = array();
             unset($lines);
             echo chr(12)."\n".$file;
             echo ',',strlen($tags),"\n",
                 $tags;
         }
+    }
+}
+
+function add_def(&$defs, $def) {
+    $defs[$def['name']] = ''
+        .$def['search'].chr(127)
+        .$def['name'].chr(1)
+        .$def['line'].','
+        .$def['offset']."\n";
+
+    if ($def['ns-name']) {
+        $defs[$def['ns-name']] = ''
+            .$def['search'].chr(127)
+            .$def['ns-name'].chr(1)
+            .$def['line'].','
+            .$def['offset']."\n";
+
     }
 }
